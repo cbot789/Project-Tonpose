bd4eef611b7e5e32c264fc207409c92cc0ddd95a
Luke Oetken <theluke@iastate.edu>
2016-10-13 17:17:54 -0500

    Added simple libgdx game test drop_project

---
+++ Drop_project/core/src/com/badlogic/drop/Drop.java
@@ -0,0 +1,134 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
+
+public class Drop extends ApplicationAdapter {
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
+
+	private Array<Rectangle> raindrops;
+	private long lastDropTime; //time in ns
+
+	@Override
+	public void create () {
+		// load images for droplet and bucket
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// load drop sound and rain music
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// start music
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// new camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		batch = new SpriteBatch();
+
+		// initialize the bucket
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+		// add raindrops
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+
+	}
+
+	@Override
+	public void render () {
+		// clear screen to dark blue color
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+
+		// render bucket and raindrops
+		batch.setProjectionMatrix(camera.combined); // tells spriteBatch to use camera coordinate system
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops){
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		batch.end();  // submits all drawing requests between begin() and end() at once. Speeds up OpenGL rendering
+
+		// make bucket move on touch
+		if(Gdx.input.isTouched()){
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos); // transforms the coordinates of the vector to the coordinate system of the camera
+			bucket.x = touchPos.x - 64 / 2;
+		}
+
+		// make sure bucket stays in screen
+		if(bucket.x < 0)
+			bucket.x = 0;
+		if(bucket.y > 800 - 64)
+			bucket.y = 800 - 64;
+
+		// spawn raindrop if enough time has passed
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000)
+			spawnRaindrop();
+
+		// move raindrops, remove if below screen or in bucket
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while(iter.hasNext()){
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.overlaps(bucket)){
+				dropSound.play();
+				iter.remove();
+			}
+			if(raindrop.y + 64 < 0)
+				iter.remove();
+		}
+
+	}
+
+	private void spawnRaindrop(){
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800 - 64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
+	
+	@Override
+	public void dispose () {
+		//dispose assets
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+		batch.dispose();
+	}
+}
