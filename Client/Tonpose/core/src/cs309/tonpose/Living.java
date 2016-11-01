00d40c13f887c5809b92d7d31bda83c7dfa99057
Quade Spellman <quade@iastate.edu>
2016-11-01 17:36:05 -0500

    moved mob to its class and moved textures to thier classes

--- Client/Tonpose/core/src/cs309/tonpose/Living.java
+++ Client/Tonpose/core/src/cs309/tonpose/Living.java
@@ -8,6 +8,11 @@ public class Living extends Entity {
     protected boolean destory;
     protected int lvl;
 
+    //Entity(int locationX, int locationY, int id, int maxHp, int height, int width, int mass, int invSize, boolean killable, boolean collision){
+    public Living(int x, int y, int hp, int height, int width, int invSize) {
+        super(x, y, -1, hp, height, width, 1, invSize, true, true);
+    }
+
     public void move(float x, float y){
         AI.direct(x, y, this.getRectangle());
     }
--- Client/Tonpose/core/src/cs309/tonpose/Tree.java
+++ Client/Tonpose/core/src/cs309/tonpose/Tree.java
@@ -1,5 +1,8 @@
 package cs309.tonpose;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Created by Caleb on 11/1/2016.
  */
@@ -8,6 +11,7 @@ public class Tree extends Entity { //id is 1
 
     public Tree(int locationX, int locationY, int id){
         super(locationX, locationY, id, 100, 100, 50, 1000, 0, true, true);
+        texture = new Texture(Gdx.files.internal("treeStill.png"));
     }
 
     @Override
